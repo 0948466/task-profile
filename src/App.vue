03c10ffa25946b6ed170e18cd04c466ef54bea19
Evgenii Grishin <0948466@gmail.com>
2018-07-23 20:01:26 +0300

    доб-е полифилла

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app">
-    <profile class="profile"></profile>
-  </div>
+    <div id="app">
+        <profile class="profile"></profile>
+    </div>
 </template>
 
 <script>
   import Profile from "@/components/Profile";
+  import polyfillClosest from '@/js/polyfill'
 
   export default {
     name: 'app',
+    beforeMount() {
+      polyfillClosest((Element.prototype))
+    },
     components: {
       Profile
     }
@@ -16,22 +20,32 @@
 </script>
 
 <style lang="scss" scoped>
-  @import "scss/mixins.scss";
+    @import "scss/mixins.scss";
 
-  #app {
-    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    background-color: $color1;
-    @include responsiveFont(18px, 15px);
-  }
+    #app {
+        display: flex;
+        align-items: center;
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        background-color: $color1;
+        @include responsiveFont(18px, 15px);
+        min-width: 320px;
+        min-height: 100vh;
+    }
 
-  .profile {
-    margin: 0 auto;
-    max-width: 980px;
-    min-width: 320px;
-    width: 100%;
-    box-sizing: border-box;
-  }
+    .profile {
+        margin: 0 auto;
+        max-width: 980px;
+        min-width: 320px;
+        width: 100%;
+        box-sizing: border-box;
+    }
+</style>
+
+<style>
+    input::-ms-clear {
+        display: none;
+    }
 </style>
